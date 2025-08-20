4928c822c53b5574a4c6e8fe0bfe3aa706710278
Shruti441 <varshneyshruti2003@gmail.com>
2025-08-20 10:22:05 +0530

    commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use('/', schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
